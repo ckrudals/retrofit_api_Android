c3566cb1303e5433a724e5e967daaeaaad81ba07
ckrudals <2004ckck@naver.com>
2021-02-14 23:40:56 +0900

    2021-02-14

--- app/src/main/java/com/example/jetpack_practice/retrofit/IRetrofit.kt
+++ app/src/main/java/com/example/jetpack_practice/retrofit/IRetrofit.kt
@@ -1,14 +1,18 @@
 package com.example.jetpack_practice.retrofit
 
+import com.example.jetpack_practice.utils.Api
+import retrofit2.Call
+import com.google.gson.JsonElement
+
 import retrofit2.http.GET
 import retrofit2.http.Query
 
 interface IRetrofit {
 
-    @GET("/search/photos")
-    fun searchPhotos(@Query("query") searchTerm: String)
+    @GET(Api.SEARCH_PHOTOS) //API KEY
+    fun searchPhotos(@Query("query") searchTerm: String): Call<JsonElement>
 
-    @GET(" /search/users")
-    fun searchUsers(@Query("query") searchTerm: String)
+    @GET(Api.SEARCH_USERS) //API KEY
+    fun searchUsers(@Query("query") searchTerm: String): Call<JsonElement>
 
 }
